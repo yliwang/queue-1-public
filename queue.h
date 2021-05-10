85dd4bbb389e2e3699ba60acee6700f78c35075f
yliwang <1220264221@qq.com>
2021-05-10 10:55:26 +0800

    Update queue.h

--- queue.h
+++ queue.h
@@ -4,14 +4,12 @@
 
 struct queue
 {
-	T *store;
-	int capacity;
+	T store[#define MAX_ELE];
 	int count;
 	int front_index;
 };
 
-struct queue *init_queue(struct queue *q, int init_cap);
-void uninit_queue(struct queue *q);
+struct queue *init_queue(struct queue *q);
 
 struct queue *enter(struct queue *q, T e);
 T leave(struct queue *q);
